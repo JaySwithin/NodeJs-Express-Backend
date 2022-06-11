de4cf1e02e84da5953a6601ed1b9f6117851c0e8
Swithin Okyere <jayswithin@gmail.com>
2022-06-11 18:49:54 +0000

    [UPDATE] - Redirect all unexpected routes to the home route

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get("/", (req, res) => {
   res.send("Express server running now.");
 });
 
+
+// All remaining requests return to the home route.
+app.get("/*", (req, res) => {
+  res.status(404).redirect(301, "/");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
