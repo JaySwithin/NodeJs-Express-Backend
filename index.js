6a7227a357d7a69b5ff29e18655a4771570d08db
Swithin Okyere <jayswithin@gmail.com>
2022-06-12 22:34:14 +0000

    [UPDATE] - Middlewares added

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler")
 const PORT = process.env.PORT || 8000;
+
+// Middlewares
+app.use(express.urlencoded({ extended: false })); // to handle urlncoded data(form data)
+app.use(express.json());
+app.use(cors());
+
+// Custom middleware logger
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Express server running now.");
 });
 
-
 // All remaining requests return to the home route.
-app.get("/*", (req, res) => {
+app.all("*", (req, res) => {
   res.status(404).redirect(301, "/");
+  
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
