f572644ea4ff1a05f15985a10c796b03102c994e
Swithin Okyere <jayswithin@gmail.com>
2022-06-17 22:24:35 +0000

    [UPDATE] - Controller for employees done, routes fixed

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,15 +1,69 @@
-const data = {};
-data.employees = require("../model/employees.json");
+const Employee = require("./../model/Employee");
 
-const getAllEmployees = (req, res) => {
-  res.json(data.employees)
+const getAllEmployees = async (req, res) => {
+  const employees = await Employee.find();
+  if (!employees)
+    return res.status(204).json({ message: "No employees found" });
+  res.json(employees);
+};
+
+const createEmployee = async (req, res) => {
+  if (!req?.body?.firstname || !req?.body?.lastname) {
+    return res
+      .status(400)
+      .json({ "message": "First and last name are required" });
+  }
+
+  try {
+    const result = await Employee.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+    res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateEmployee = async (req, res) => {
+  if (!req?.body?.id) {
+    return res.status(400).json({ "message": "ID is required" });
+  }
+
+  const employee = await Employee.findOne({ _id: req.body.id }).exec();
+  if(!employee) {
+    return res.status(204).json({ "message": `Employee ID ${req.body.id} doesn't exist.` });
+  }
+
+  if(req?.body?.firstname) employee.firstname === req.body.firstname;
+  if(req?.body?.firstname) employee.firstname === req.body.firstname;
+
+  const result = await employee.save();
+  console.log(result);
 }
 
-const createEmployee = (req, res) => {
-  res.json({
-    "firstname": req.body.firstname,
-    "lastname": req.body.lastname,
-  })
+const deleteEmployee = async (req, res) => {
+  if (!req?.body?.id) {
+    return res.status(400).json({ "message": "ID is required" });
+  }
+
+  const employee = await Employee.findOne({ _id: req.body.id }).exec();
+  if(!employee) {
+    return res.status(204).json({ "message": `Employee ID ${req.body.id} doesn't exist.` });
+  }
+  const result = await employee.deleteOne({ _id: req.body.id });
+  console.log(result);
 }
 
-module.exports = {getAllEmployees, createEmployee}
+const getEmployeeById = async (req, res) => {
+  if (!req?.params?.id) {
+    return res.status(400).json({ "message": "ID is required" });
+  }
+  const employee = await Employee.findOne({ _id: req.params.id }).exec();
+  if(!employee) {
+    return res.status(204).json({ "message": `Employee ID ${req.params.id} doesn't exist.` });
+  }
+  res.json(employee)
+}
+
+module.exports = { getAllEmployees, createEmployee, updateEmployee, deleteEmployee, getEmployeeById };
--- routes/employees.js
+++ routes/employees.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getAllEmployees, createEmployee } = require("../controllers/employees");
+const { getAllEmployees, createEmployee, updateEmployee, deleteEmployee, getEmployeeById } = require("../controllers/employees");
 const ROLES_LIST = require('../config/roles_list');
 const verifyRoles = require('../middleware/verifyRoles');
 
 router.route("/")
   .get(getAllEmployees)
   .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), createEmployee)
+  .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), updateEmployee)
+  .delete(verifyRoles(ROLES_LIST.Admin), deleteEmployee);
+
+router.route('/:id')
+    .get(getEmployeeById);
 
 module.exports = router;
