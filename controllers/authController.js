9932498cbb22537868cec89019f5753fcab5fcfc
Swithin Okyere <jayswithin@gmail.com>
2022-06-17 00:05:09 +0000

    [UPDATE] - Updated controllers to use mongodb instead of local json

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,7 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-
+const User = require("../model/User")
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const fsPromises = require("fs").promises;
-const path = require("path");
 
 const handleLogin = async (req, res) => {
   let { username, password } = req.body;
@@ -19,9 +11,8 @@ const handleLogin = async (req, res) => {
     });
   }
 
-  const foundUser = usersDB.users.find((person) => person.username === username);
+  const foundUser = await User.findOne({ username }).exec();
   if (!foundUser) return res.sendStatus(401);
-
   const match = await bcrypt.compare(password, foundUser.password)
 
   if (match) {
@@ -43,14 +34,11 @@ const handleLogin = async (req, res) => {
     )
     
     // Saving refresh token with current user
-    const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username)
-    const currentUser = { ...foundUser, refreshToken }
-    usersDB.setUsers([...otherUsers, currentUser])
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    )
-    res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000 })
+    foundUser.refreshToken = refreshToken;
+    const result = await foundUser.save();
+    console.log(result);
+
+    res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }) // secure: true, in prod.
     res.json({ accessToken })
   } else {
     res.sendStatus(401)
--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,12 +1,4 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-const fsPromises = require("fs").promises;
-const path = require("path");
-
+const User = require("../model/User");
 
 const handleLogout = async (req, res) => {
   // On client side, also delete the access token.
@@ -15,20 +7,17 @@ const handleLogout = async (req, res) => {
   const refreshToken = cookies.jwt;
 
   // Check if refresh token is in database
-  const foundUser = usersDB.users.find((person) => person.refreshToken === refreshToken);
+  const foundUser = await User.findOne({ refreshToken }).exec();
   if (!foundUser) {
     res.clearCookie("jwt", { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
     return res.sendStatus(204); // Request successful, no content to send back
   }
 
   // Delete the refresh token in database
-  const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
-  const currentUser = {...foundUser, refreshToken: '' };
-  usersDB.setUsers([...otherUsers, currentUser]);
-  await fsPromises.writeFile(
-    path.join(__dirname, "..", "model", "users.json"),
-    JSON.stringify(usersDB.users)
-  )
+  foundUser.refreshToken = "";
+  const result = await foundUser.save();
+
+  console.log(result)
 
   res.clearCookie("jwt", { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000 }) // add secure: true - which serves on https.
   res.sendStatus(204);
